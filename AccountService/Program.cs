a0e5fb8baed677a28b0ff4fd35aaf44e59b79994
Timofey-Vodyanov <timofeyvodyanov@gmail.com>
2026-02-14 23:46:44 +0300

    Initial commit: created ASP.NET Core Web API project with Swagger and XML docs AND configured launchSettings

--- AccountService/Program.cs
+++ AccountService/Program.cs
@@ -1,23 +1,35 @@
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Account Service API",
+        Version = "v1",
+        Description = "Мкросервис «Банковские счета»"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
