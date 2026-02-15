48e7f4ebadb658fbe8277b97fd768a066a0aa7b0
Timofey-Vodyanov <timofeyvodyanov@gmail.com>
2026-02-15 14:35:16 +0300

    Added service layer interfaces

---
+++ AccountService/Services/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using AccountService.Dtos;
+
+namespace AccountService.Services;
+
+public interface IAccountService
+{
+    Task<IEnumerable<AccountResponse>> GetAccountsAsync(Guid? ownerId);
+    Task<AccountResponse?> GetAccountByIdAsync(Guid id);
+    Task<bool> AccountExistsAsync(Guid id);
+    Task<AccountResponse> CreateAccountAsync(CreateAccountRequest request);
+    Task UpdateAccountAsync(Guid id, UpdateAccountRequest request);
+    Task DeleteAccountAsync(Guid id);
+    Task<IEnumerable<TransactionResponse>> GetAccountTransactionsAsync(Guid accountId, DateTime? from, DateTime? to);
+    Task<TransactionResponse> CreateTransactionAsync(Guid accountId, CreateTransactionRequest request);
+}
---
+++ AccountService/Services/ITransactionService.cs
@@ -0,0 +1,9 @@
+﻿using AccountService.Dtos;
+
+namespace AccountService.Services;
+
+public interface ITransactionService
+{
+    Task<TransactionResponse?> GetTransactionByIdAsync(Guid id);
+    Task TransferAsync(TransferRequest request);
+}
