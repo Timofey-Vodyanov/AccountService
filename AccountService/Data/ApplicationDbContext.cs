4269c454279c406aaa2c46fe4c3a6b1d257a4691
Timofey-Vodyanov <timofeyvodyanov@gmail.com>
2026-02-15 08:56:32 +0300

    Added ApplicationDbContext with DbSets for Account and Transaction

---
+++ AccountService/Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AccountService.Models;
+
+namespace AccountService.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Настройка индексов и отношений (опционально)
+        modelBuilder.Entity<Account>()
+            .HasIndex(a => a.OwnerId);
+
+        modelBuilder.Entity<Transaction>()
+            .HasIndex(t => t.AccountId);
+
+        // Указываем точность для decimal, чтобы избежать предупреждений в PostgreSQL
+        modelBuilder.Entity<Account>()
+            .Property(a => a.Balance)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Account>()
+            .Property(a => a.InterestRate)
+            .HasPrecision(18, 2);
+
+        modelBuilder.Entity<Transaction>()
+            .Property(t => t.Amount)
+            .HasPrecision(18, 2);
+    }
+}
